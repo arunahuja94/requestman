054fd8b9c1a93274a019c1e83ba1c978707fc642
Arun Ahuja <arunahuja94@gmail.com>
2021-06-28 02:13:02 +0530

    Ui changes and icon added

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -5,9 +5,9 @@ import "../styles/HeaderContainer.css";
 const HeaderContainer = () => {
   return (
     <div className="HeaderContainer">
-    <div class="header__section-left">
-    <div class="top-navigation">
-    <a className="site-identity" target="_self" href="https://github.com/arunahuja94/requestman"><img className="site-logo" src="https://raw.githubusercontent.com/arunahuja94/requestman/master/postman.png" /><div class="top-navigation-text">Requestman</div></a>
+    <div className="header__section-left">
+    <div className="top-navigation">
+    <a className="site-identity" target="_self" href="https://github.com/arunahuja94/requestman"><img className="site-logo" src="https://raw.githubusercontent.com/arunahuja94/requestman/master/postman.png" /><div className="top-navigation-text">Requestman</div></a>
     </div></div>
     </div>
   );
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,28 +1,26 @@
 import React, { useState, Fragment } from "react";
 
 import { requestHandler } from "../services/api";
-import Response from './Response';
+import Response from "./Response";
 import "../styles/MainWrapper.css";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-// import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import * as FaIcons from "react-icons/fa";
 
 const MainContainer = () => {
-   
-const [apiUrl,setApiUrl] = useState('');
-const [apiAction,setApiAction] = useState('get');
-const [apiResponse,setApiResponse] = useState('');
-const [inputFields, setInputFields] = useState([
-    { qp_key: '', qp_value: '' }
+  const [apiUrl, setApiUrl] = useState("");
+  const [apiAction, setApiAction] = useState("get");
+  const [apiResponse, setApiResponse] = useState("");
+  const [inputFields, setInputFields] = useState([
+    { qp_key: "", qp_value: "" },
   ]);
 
-  
   const handleAddFields = () => {
     const values = [...inputFields];
-    values.push({ qp_key: '', qp_value: '' });
+    values.push({ qp_key: "", qp_value: "" });
     setInputFields(values);
   };
 
-  const handleRemoveFields = index => {
+  const handleRemoveFields = (index) => {
     const values = [...inputFields];
     values.splice(index, 1);
     setInputFields(values);
@@ -39,47 +37,51 @@ const [inputFields, setInputFields] = useState([
     setInputFields(values);
   };
 
-const handleChange = (event) => {
+  const handleChange = (event) => {
     const target = event.target;
-    console.log(target.name,target.value);
-    switch (target.name)
-    {
-    case 'apiUrl':
-    setApiUrl(target.value);
-    break;
+    console.log(target.name, target.value);
+    switch (target.name) {
+      case "apiUrl":
+        setApiUrl(target.value);
+        break;
 
-    case 'apiAction':
-    setApiAction(target.value);
-    break;
+      case "apiAction":
+        setApiAction(target.value);
+        break;
 
-    default:
-
+      default:
     }
-  }
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     let apiData = {};
-    for (const{'qp_key':qp_key,'qp_value':qp_value} of inputFields) {
-        apiData[qp_key] = qp_value;
-      }
-     const resp = await requestHandler({'apiUrl':apiUrl,'apiAction':apiAction,'apiData':apiData});
+    for (const { qp_key: qp_key, qp_value: qp_value } of inputFields) {
+      apiData[qp_key] = qp_value;
+    }
+    const resp = await requestHandler({
+      apiUrl: apiUrl,
+      apiAction: apiAction,
+      apiData: apiData,
+    });
     setApiResponse(resp);
   };
 
-    return (
-      <div className="MainWrapper">
-          <div className="sidebar"></div>
-          <div className="MainWrapperContent">
+  return (
+    <div className="MainWrapper">
+      <div className="sidebar"></div>
+      <div className="MainWrapperContent">
         <div className="request">
           <div className="request-inner">
             <form onSubmit={handleSubmit}>
               <ul>
                 <li className="method">
                   <div>
-                    <select name="apiAction"
-                      value={apiAction} 
-                      onChange={handleChange}>
+                    <select
+                      name="apiAction"
+                      value={apiAction}
+                      onChange={handleChange}
+                    >
                       <option value="get">GET</option>
                       <option value="post">POST</option>
                       <option value="put">PUT</option>
@@ -89,12 +91,12 @@ const handleChange = (event) => {
                 </li>
                 <li className="apiUrl method">
                   <div>
-                    <input 
+                    <input
                       type="url"
                       name="apiUrl"
                       value={apiUrl}
                       onChange={handleChange}
-                      placeholder="https://httpbin.org/get" 
+                      placeholder="https://httpbin.org/get"
                       required
                       className="urlInput"
                     />
@@ -102,82 +104,89 @@ const handleChange = (event) => {
                 </li>
                 <li className="send method">
                   <div>
-                    <button class="Rbutton"> Send </button>
+                    <button className="Rbutton"> Send </button>
                   </div>
                 </li>
               </ul>
             </form>
           </div>
-        <div className="ParamTabs" >
-        <Tabs>
-    <TabList>
-      <Tab>query param</Tab>
-      <Tab>Headers</Tab>
-    </TabList>
+          <div className="ParamTabs">
+            <Tabs>
+              <TabList>
+                <Tab>Param</Tab>
+                <Tab>Headers</Tab>
+              </TabList>
 
-    <TabPanel>
-    <div className="query_param">
-            <div className="query_param_inputs">
-        {inputFields.map((inputField, index) => (
-            <Fragment key={`${inputField}~${index}`}>
-              <div className="form-container">  
-              <div className="qp-form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="qp_key"
-                  name="qp_key"
-                  value={inputField.qp_key}
-                  placeholder="key"
-                  onChange={event => handleInputChange(index, event)}
-                />
-              </div>
-              <div className="qp-form-group">
-                <input
-                  type="text" 
-                  className="form-control" 
-                  id="qp_value"
-                  name="qp_value"
-                  value={inputField.qp_value}
-                  placeholder="Value"
-                  onChange={event => handleInputChange(index, event)}
-                />
-              </div>
-              <div className="form-group width-15">
-                <button
-                  className="btn btn-link"
-                  type="button"
-                  onClick={() => handleRemoveFields(index)}
-                >
-                Remove
-                </button>
-              </div>
-              </div>
-            </Fragment>
-          ))}
+              <TabPanel>
+                <div className="query_param">
+                  <div className="query_param_inputs">
+                    <div className="TabContentLabelContainer">
+                      <label className="TabContentLabel">Query Params</label>
+                      <span onClick={() => handleAddFields()}>
+                        <FaIcons.FaPlus
+                          size="0.8em"
+                          color="#ff5722"
+                          className="AddIcon"
+                        />
+                      </span>
+                    </div>
+
+                    {inputFields.map((inputField, index) => (
+                      <Fragment key={`${inputField}~${index}`}>
+                        <div className="form-container">
+                          <div className="qp-form-group">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="qp_key"
+                              name="qp_key"
+                              value={inputField.qp_key}
+                              placeholder="key"
+                              onChange={(event) =>
+                                handleInputChange(index, event)
+                              }
+                            />
+                          </div>
+                          <div className="qp-form-group">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="qp_value"
+                              name="qp_value"
+                              value={inputField.qp_value}
+                              placeholder="Value"
+                              onChange={(event) =>
+                                handleInputChange(index, event)
+                              }
+                            />
+                          </div>
+                          <div className="form-group width-15">
+                            <span onClick={() => handleRemoveFields(index)}>
+                              <FaIcons.FaTrashAlt
+                                size="0.8em"
+                                color="#ff5722"
+                                className="TrashIcon"
+                              />
+                            </span>
+                          </div>
+                        </div>
+                      </Fragment>
+                    ))}
+                  </div>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <h2>Any content 2</h2>
+              </TabPanel>
+            </Tabs>
           </div>
-                  <button
-                  className="Addbutton"
-                  type="button"
-                  onClick={() => handleAddFields()}
-                >
-                  Add
-                </button>
-        </div>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
-        </div>
-
         </div>
         <div className="response">
-          <Response data={ apiResponse } />
-        </div>
+          <Response data={apiResponse} />
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default MainContainer;
